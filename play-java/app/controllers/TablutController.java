45ff454638a47c0ff391ce44499315049f53cd85
WaldemarBrill <w.brill1@googlemail.com>
2016-12-08 13:46:57 +0100

    adding -gitignore with *.log

--- play-java/app/controllers/TablutController.java
+++ play-java/app/controllers/TablutController.java
@@ -3,11 +3,18 @@ package controllers;
 import play.mvc.*;
 
 import views.html.*;
+import de.htwg.se.tablut.Tablut;
+import de.htwg.se.tablut.aview.TextUI;
+import de.htwg.se.tablut.bcontroller.IController;
 
 public class TablutController extends Controller {
-
+	Tablut tablut = Tablut.getInstance();
+	TextUI tui = tablut.getTui();
+	IController controller = tablut.getController();
+	
     public Result game() {
-        return ok(tablut.render("Bist jetzt in game dude."));
+    	tui.funktion("9");
+        return ok(game.render(controller));
     }
     
     public Result help() {
