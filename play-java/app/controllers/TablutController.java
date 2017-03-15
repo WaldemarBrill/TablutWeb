5545778097d7e57148b270022f4f1331b6e76caf
WaldemarBrill <w.brill1@googlemail.com>
2017-03-15 08:25:31 +0100

    changes

--- play-java/app/controllers/TablutController.java
+++ play-java/app/controllers/TablutController.java
@@ -8,20 +8,23 @@ import de.htwg.se.tablut.aview.TextUI;
 import de.htwg.se.tablut.bcontroller.IController;
 
 public class TablutController extends Controller {
-	Tablut tablut = Tablut.getInstance();
-	TextUI tui = tablut.getTui();
-	IController controller = tablut.getController();
+	static Tablut tablut = Tablut.getInstance();
 	
     public Result game() {
-    	tui.funktion("9");
-        return ok(game.render(controller));
+    	tablut.getInstance().getTui().funktion("9");
+        return ok(game.render(tablut.getInstance().getTui().buildString(), tablut.getController()));
+    }
+    
+    public Result command(String line) {
+    	tablut.getInstance().getTui().funktion(line);
+    	return ok(game.render(tablut.getInstance().getTui().buildString(), tablut.getController()));
     }
     
     public Result help() {
-        return ok(help.render("Bist jetzt in help dude."));
+        return ok(help.render("Help menu"));
     }
     
     public Result controls() {
-        return ok(controls.render("Bist jetzt in controls dude"));
+        return ok(controls.render("Controls menu"));
     }
 }
